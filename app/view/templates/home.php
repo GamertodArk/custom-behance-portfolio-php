53c3b9695b5e6b70ba922d3a5b94fc19de7a3872
Elvis Garcia <cavesagarcia@hotmail.com>
2018-11-16 17:57:07 -0400

    Creating the view

--- app/view/templates/home.php
+++ app/view/templates/home.php
@@ -1,3 +1,10 @@
-<?php  
-	echo 'Hello World';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Custom Behance Portfolio</title>
+</head>
+<body>
+	<h2>Hello World</h2>
+</body>
+</html>
